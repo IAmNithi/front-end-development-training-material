9dd7285e15b7efa965d5e1f221edec4f47d8232a
Kalanithi Sethuraman <kalanithi_sas@outlook.com>
2019-12-16 14:00:45 +0530

    "added js dom add and remove functions"

--- HTML&CSS/script.js
+++ HTML&CSS/script.js
@@ -1,3 +1,16 @@
-function printMessage(){
-    console.log('printing message in console on click');
+function printMessage(name, company){
+    console.log('printing message in console on click', name, company);
+    var daString="<li "+ 'id="'+ name + '"' +" class=\'list-group-item\' onclick=\'removeList(event)\'>"+ name +' from '+ company +"</li>";
+    var daParent=document.getElementById("list");
+    daParent.innerHTML+=daString;
+}
+
+function removeList(event){
+    // console.log(event.target);
+    // var list = event.target.id;
+    // console.log(list);
+    // var daParent=document.getElementById("list");
+    // daParent.getElementById("list").remove();
+    var elem = document.getElementById(event.target.id);
+    elem.parentNode.removeChild(elem);
 }
