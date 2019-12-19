cfa579af6e6194ccae43b40bc14e2c69ea2e3182
Kalanithi Sethuraman <kalanithi_sas@outlook.com>
2019-12-19 13:55:47 +0530

    updated stringReverse.js

--- JavaScript Problem Solving/stringReverse.js
+++ JavaScript Problem Solving/stringReverse.js
@@ -6,13 +6,11 @@ console.log(stringReverse('kalanithi'));
 
 
 function stringReverse2(param){
-    var a = param.split('');
-    for(var i=0;i<a.length;i++){
-        var b = a[i];
-        a[i].splice(0,1);
-        a[a.length+1].push(b);
+    var a = '';
+    for(var i = param.length - 1; i >= 0; i--){
+        a+= param[i];
     }
-    return a.join('');
+    return a;
 }
 
-console.log(stringReverse2('tenet'));
+console.log(stringReverse2('jack'));
