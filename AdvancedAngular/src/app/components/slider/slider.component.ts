40387b996ac3d9a480b70118aa0b3327088f6bfc
Kalanithi Sethuraman <kalanithi_sas@outlook.com>
2020-01-10 18:04:04 +0530

    Added API to the Slider

--- AdvancedAngular/src/app/components/slider/slider.component.ts
+++ AdvancedAngular/src/app/components/slider/slider.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-slider',
@@ -6,10 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./slider.component.css']
 })
 export class SliderComponent implements OnInit {
-
+  @Input() sliderImage: any;
   constructor() { }
 
   ngOnInit() {
+    console.log(this.sliderImage);
   }
 
 }
